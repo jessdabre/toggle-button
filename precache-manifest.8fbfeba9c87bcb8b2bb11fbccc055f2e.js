2bf6b251cc43e2dd97a5068cbce1a5fe76642ab6
Jess Dabre <jessdabre14@gmail.com>
2024-11-10 16:05:10 +0530

    Updates

---
+++ precache-manifest.8fbfeba9c87bcb8b2bb11fbccc055f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cff0c3b49f157efefe5cb04e8bb64de",
+    "url": "/toggle-button/index.html"
+  },
+  {
+    "revision": "e869575a1a3eeac2c5d8",
+    "url": "/toggle-button/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/toggle-button/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/toggle-button/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "e869575a1a3eeac2c5d8",
+    "url": "/toggle-button/static/js/main.dd00306e.chunk.js"
+  },
+  {
+    "revision": "2b1ab6ffd847dd129adc",
+    "url": "/toggle-button/static/js/runtime~main.3214b236.js"
+  }
+]);

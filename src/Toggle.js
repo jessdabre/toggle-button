dc6b5df6a98b842393808bf1bf9fc5d1451fab64
Jess Dabre <jessdabre14@gmail.com>
2024-11-10 16:31:55 +0530

    second commit

--- src/Toggle.js
+++ src/Toggle.js
@@ -4,7 +4,7 @@ class Toggle extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.props.handleToggle}>Click me</button>
+        <button onClick={this.props.handleToggle}>Click on this button</button>
       </div>
     );
   }
